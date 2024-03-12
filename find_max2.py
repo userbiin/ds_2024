2fa05e625057fd6dcdb71948d75e19f31126ac32
userbiin <162470759+userbiin@users.noreply.github.com>
2024-03-12 14:53:16 +0000

    Fix issue with find_max2.py

---
+++ find_max2.py
@@ -0,0 +1,16 @@
+def find_max_iterative(numbers):
+    max_value=numbers[0]
+    for i in range(10):
+        if numbers[i] > max_value:
+            max_value = numbers[i]
+
+    return max_value
+
+
+numbers = []
+for i in range(10):
+    num = int(input("숫자를 입력하세요: "))
+    numbers.append(num)
+
+max_value = find_max_iterative(numbers)
+print("최댓값: ", max_value)
