0e5dcf38b703b1ad26e132c5851e5305db84cf74
userbiin <162470759+userbiin@users.noreply.github.com>
2024-04-14 13:43:24 +0000

    mod

--- heap/MinheapDemo.py
+++ heap/MinheapDemo.py
@@ -1,18 +1,38 @@
-from lfuheap import minHeap
+from minheap import minHeap
 
-h1 = minHeap([1, 11, 9, 2, 3])
-h1.buildHeap()
-h1.headPrint()
-h1.insert(7)
-h1.insert(5)
-h1.insert(9)
-h1.insert(4)
-h1.insert(11)
-h1.insert(19)
-h1.insert(20)
-h1.insert(21)
-h1.insert(11)
-h1.headPrint()
-h1.deleteMin()
-h1.headPrint()
+class LPN_Frequency: 
+    def __init__(self, lpn, frequency):
+        self.lpn = lpn
+        self.frequency = frequency
+
+    def __repr__(self):
+        return f"LPN: {self.lpn}, Frequency: {self.frequency}"
+
+
+# minHeap 클래스 테스트를 위한 테스트 코드
+if __name__ == "__main__":
+    # minHeap 객체 생성
+    h1 = minHeap([])
+    
+    # 초기 힙 상태 출력
+    print("Initial Heap:")
+    h1.headPrint()
+    
+    # 데이터 삽입
+    h1.insert(LPN_Frequency(7, 3))
+    h1.insert(LPN_Frequency(5, 1))
+    h1.insert(LPN_Frequency(9, 2))
+    
+    # 데이터 삽입 후 힙 상태 출력
+    print("Heap after insertions:")
+    h1.headPrint()
+    
+    # 최소값 삭제
+    min_value = h1.deleteMin()
+    print("Min value deleted:", min_value.lpn, min_value.frequency)
+    
+    # 최소값 삭제 후 힙 상태 출력
+    print("Heap after deletion:")
+    h1.headPrint()
+
 
--- heap/newlfu.py
+++ heap/newlfu.py
@@ -5,6 +5,7 @@ class LPN_Frequency:
         self.lpn = lpn
         self.frequency = frequency
 
+
 class LFUSimulator:
     def __init__(self):
         self.cache = {}
@@ -12,39 +13,41 @@ class LFUSimulator:
         self.cache_hit = 0
         self.tot_cnt = 0
 
-    def lfu_sim(self, cache_slots):
-        self.cache_slots = cache_slots
-        data_file = open("/workspaces/ds_2024/heap/linkbench.trc")
-        
-        for line in data_file.readlines():
-            lpn = line.split()[0]  # LPN을 추출
-            self.tot_cnt += 1
+    def lfu_sim(self, cache_slots, lpn):
+        self.cache_slots = cache_slots  # LPN을 추출
+        self.tot_cnt += 1
 
-            if lpn in self.cache and self.cache[lpn]:  # cache에 호출된 페이지가 있는 경우
-                self.cache_hit += 1
-                lpn_frequency = self.cache[lpn]
-                lpn_frequency.frequency += 1
-                self.heap.update(lpn_frequency)  
+        if self.cache[lpn]:  # cache에 호출된 페이지가 있는 경우
+            self.cache_hit += 1
+            lpn_frequency = self.cache[lpn]
+            lpn_frequency.frequency += 1
+            self.heap.update(lpn_frequency)  
 
+        else: 
+            if len(self.cache) < cache_slots:  #cache에 빈 자리가 있다면 
+                lpn_frequency = LPN_Frequency(lpn, 1)
+                self.cache[lpn] = lpn_frequency
+                self.heap.insert(lpn_frequency)
+            
             else: 
-                if len(self.cache) < cache_slots:  #cache에 빈 자리가 있다면 
-                    lpn_frequency = LPN_Frequency(lpn, 1)
-                    self.cache[lpn] = lpn_frequency
-                    self.heap.insert(lpn_frequency)
-                
-                else: 
-                    lfu_heap = self.heap.min()
-                    del self.cache[lfu_heap.lpn]
-                    self.heap.deleteMin()
-                    lpn_frequency = LPN_Frequency(lpn, 1)
-                    self.cache[lpn] = lpn_frequency
-                    self.heap.insert(lpn_frequency)
+                lfu_heap = self.heap.min()
+                del self.cache[lfu_heap.lpn]
+                self.heap.deleteMin()
+                lpn_frequency = LPN_Frequency(lpn, 1)
+                self.cache[lpn] = lpn_frequency
+                self.heap.insert(lpn_frequency)
 
+    def print_stats(self):
         hit_ratio = self.cache_hit / self.tot_cnt   
         print("cache_slot =", self.cache_slots, "cache_hit =", self.cache_hit, "hit ratio =", hit_ratio)
 
 
 if __name__ == "__main__":
     simulator = LFUSimulator()
+    data_file = open("/workspaces/ds_2024/heap/linkbench.trc")
+    lines = data_file.readlines()
     for cache_slots in range(100, 1000, 100):
-        simulator.lfu_sim(cache_slots)
+        for line in lines:
+            lpn = line.split()[0]
+            simulator.lfu_sim(cache_slots, lpn)
+        simulator.print_stats()
